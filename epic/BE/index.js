cf389a344131d7107260159565a0f6c929fea38f
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2021-08-08 12:54:07 -0400

    set up express with endpoint route that triggers email. Currently receiving email though obj is undefined - working through bug

--- epic/BE/index.js
+++ epic/BE/index.js
@@ -1,116 +1,167 @@
-const PORT = process.env.PORT || 587
 const express = require("express");
-var router = express.Router();
-// const app = express();
-const nodemailer = require("nodemailer");
-const cors = require("cors");
 const creds = require('./config');
-// require("dotenv").config();
+const PORT = creds.PORT || 3001
+const nodemailer = require("nodemailer");
+const bodyParser = require('body-parser');
+
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+
+//body parser middleware
+app.use(bodyParser.json());
+// app.use(bodyParser.text({type: 'text/xml'}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors()); 
+
+//static folder
+// app.use(express.static(path.join(__dirname,'public')));
 
 
-let transporter = nodemailer.createTransport({
-  name: "smtp.gmail.com",
-  host: "smtp.gmail.com", 
-  port: PORT,
-  secure: false,
-  requireTLS: true,
-  auth: {
-    user: creds.USER,
-    pass: creds.PASS
-  }
+//Routing
+
+app.get('/', (req, res) => {
+  res.render('index', { 
+    title: 'Home Page' });
 });
 
-// const transporter = nodemailer.createTransport(transport)
+// app.get('/contact', (req, res) => {
+//   res.render('contact', { 
+//     title: 'Contact Page' });
+// })
 
-// verify connection configuration
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
-});
+//not in use at the moment
+// if (process.env.NODE_ENV === 'production'){
 
-router.post('/send', (req, res, next) => {
-  var name = req.body.name
-  var phone = req.body.phone
-  // var email = req.body.email
-  var subject = "Mail From Contact Form"
-  // var message = req.body.message
 
-  var mail = {
-    from: name,
-    to: "rossicandace85@gmail.com",
-    subject: subject,
-    phone: phone,
-    // text: message
-  }
+  //keep
+  // app.use(express.static(path.join(__dirname, 'public')));
 
-  transporter.sendMail(mail, (err, data) => {
-    console.log(mail, "body of mail")
-    if (err) {
-      res.json({
-        status: 'fail'
-      })
-    } else {
-      res.json({
-       status: 'success'
-      })
+  // app.get('*', function(req, res){
+  //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  // })
+
+
+// }
+
+//not in use at the moment
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000/send',
+//     credentials: true,
+//   })
+// );
+
+//keep
+// app.use((request, response, next) => {
+//   response.header("Access-Control-Allow-Origin", "*");
+//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+
+//keep
+//use npm start. if error starting delete node and pkg-lock and npm install again
+// var transporter = nodemailer.createTransport({
+//   name: "smtp.gmail.com",
+//   host: "smtp.gmail.com", 
+//   port: PORT,
+//   secure: true,
+//   requireTLS: true,
+//   auth: {
+//     user: creds.USER,
+//     pass: creds.PASS
+//   }
+// });
+
+  // verify connection configuration
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Server is ready to take our messages");
+//   }
+// });
+
+
+app.post('/send', (req, res) => {
+  let data = req.body 
+  console.log("alligator data", data)
+  let smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    port: 465,
+    auth: {
+      user: creds.USER,
+      pass: creds.PASS
     }
   })
+
+
+let mailOptions = {
+  from: "rossicandace85@gmail.com",
+  to: "rossicandace85@gmail.com",
+  subject: `Message from ${data.name}`,
+  html: `
+  <h3>Information</h3>
+  <ul>
+  <li>Name: ${data.name}</li>
+  <li>Phone: ${data.phone}</li>
+  <li>email: ${data.email}</li>
+  <h3>Message</h3>
+  <p> name: ${data.name} \n  email: ${data.email} \n ${data.phone}</p>
+  </ul>
+  `
+};
+
+smtpTransport.sendMail(mailOptions, (error, res) => {
+  if(error){
+    res.send(error)
+  }
+  else{
+    res.send("success")
+  }
 })
 
+smtpTransport.close();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/', router)
+})
 
-app.use((request, response, next) => {
-  response.header("Access-Control-Allow-Origin", "*");
-  response.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+//keep
+// app.post('/send', async (req, res, next) => {
+  
+//   var data = req.body;
+//   console.log("this is the data alligator", JSON.stringify(data));
+//   var to = "rossicandace85@gmail.com"
+//   var subject = "Mail From Contact Form"
+//   // var name = `${data.name}`
+//   // var email = `${data.email}`
+//   // var phone = `${data.phone}`
+//   var message = `${subject} \n  name: ${data.name} \n  email: ${data.email} \n ${data.phone}`
 
 
-app.listen(PORT, () => console.log("listening on port 587"));
+// var mail = {
+//     from: to,
+//     to: to,
+//     subject: subject,
+//     text: message
+//   }
 
-// const path = require("path");
+// transporter.sendMail(mail, (err, data) => {
+//     if (err) {
+//       res.json({
+//         status: 'fail'
+//       })
+//     } else {
+//       res.json({
+//        status: 'success'
+//       })
+//     }
+//   });
 
-// app.use(express.json())
-// app.use(express.urlencoded({extended:false}));
-
-// app.use(express.static(path.join(__dirname, "public")));
-
-// app.get("/api", (request, response) => {
-//     response.json({message: "hello from server!"})
-// });
-// app.post("/api/form", (request, response) => {
-//     const transporter = nodemailer.createTransport({
-//        service: "Gmail",
-//         auth: {
-//             user: "candacerossi85@gmail.com", //this should be your gmail account
-//             pass: "12345"
-//         }
-//     });
-//     const textBody = `FROM: ${request.body.name}; PHONE: ${request.body.phone}`;
-//     var htmlBody = `<h2> Mail From Contact Form </h2> <p>from: ${request.body.name} <a href='mailto: ${request.body.email}'>${request.body.email}</a></p>`;
-//     var mail = {
-//         from: request.body.name, //this should be your account
-//         to: "rossicandace85@gmail.com", // this could be anyone's email
-//         subject: "Mail From Contact Form",
-//         text: textBody,
-//         html: htmlBody
-//     };
-//     transporter.sendMail(mail, (err, info) => {
-//         if (err) {
-//             console.log(err);
-//             response.json({message: "an error occured; check the server's console log"});
-//         }
-//         else {
-//             response.json({message: `message sent with ID: ${info.messageId}`});
-//         }
-//     });
+//   // transporter.close();
 // });
 
-// app.listen(PORT, () => console.log("listening on port 8000"));
+
+
+app.listen(PORT, () => console.log("listening on port 3001"));
