26919b78e657f7d22e490557cd5962c026b37545
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2021-09-08 12:07:50 -0400

    removed bodyparser to correct undefined data in email bug, email sent from homepage contact form with accurate data. Need to add sent message with set timeout for ui experience

--- epic/BE/index.js
+++ epic/BE/index.js
@@ -2,92 +2,30 @@ const express = require("express");
 const creds = require('./config');
 const PORT = creds.PORT || 3001
 const nodemailer = require("nodemailer");
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const app = express();
 const cors = require('cors');
-
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
-//body parser middleware
-app.use(bodyParser.json());
-// app.use(bodyParser.text({type: 'text/xml'}));
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors()); 
 
-//static folder
-// app.use(express.static(path.join(__dirname,'public')));
-
+//body parser middleware
+// app.use(bodyParser.urlencoded({extended:true}));
+// app.use(bodyParser.json());
 
 //Routing
-
 app.get('/', (req, res) => {
   res.render('index', { 
     title: 'Home Page' });
 });
 
-// app.get('/contact', (req, res) => {
-//   res.render('contact', { 
-//     title: 'Contact Page' });
-// })
-
-//not in use at the moment
-// if (process.env.NODE_ENV === 'production'){
-
-
-  //keep
-  // app.use(express.static(path.join(__dirname, 'public')));
-
-  // app.get('*', function(req, res){
-  //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-  // })
-
-
-// }
-
-//not in use at the moment
-// app.use(
-//   cors({
-//     origin: 'http://localhost:3000/send',
-//     credentials: true,
-//   })
-// );
-
-//keep
-// app.use((request, response, next) => {
-//   response.header("Access-Control-Allow-Origin", "*");
-//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-
-//keep
-//use npm start. if error starting delete node and pkg-lock and npm install again
-// var transporter = nodemailer.createTransport({
-//   name: "smtp.gmail.com",
-//   host: "smtp.gmail.com", 
-//   port: PORT,
-//   secure: true,
-//   requireTLS: true,
-//   auth: {
-//     user: creds.USER,
-//     pass: creds.PASS
-//   }
-// });
-
-  // verify connection configuration
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log("Server is ready to take our messages");
-//   }
-// });
-
-
 app.post('/send', (req, res) => {
-  let data = req.body 
-  console.log("alligator data", data)
+  console.log(req.body)
+  let newData = req.body 
+  
+  console.log("alligator data", newData)
   let smtpTransport = nodemailer.createTransport({
     service: 'Gmail',
     port: 465,
@@ -101,15 +39,15 @@ app.post('/send', (req, res) => {
 let mailOptions = {
   from: "rossicandace85@gmail.com",
   to: "rossicandace85@gmail.com",
-  subject: `Message from ${data.name}`,
+  subject: `Message from ${newData.name}`,
   html: `
   <h3>Information</h3>
   <ul>
-  <li>Name: ${data.name}</li>
-  <li>Phone: ${data.phone}</li>
-  <li>email: ${data.email}</li>
+  <li>Name: ${newData.name}</li>
+  <li>Phone: ${newData.phone}</li>
+  <li>email: ${newData.email}</li>
   <h3>Message</h3>
-  <p> name: ${data.name} \n  email: ${data.email} \n ${data.phone}</p>
+  <p> name: ${newData.name} \n  email: ${newData.email} \n ${newData.phone}</p>
   </ul>
   `
 };
@@ -126,42 +64,6 @@ smtpTransport.sendMail(mailOptions, (error, res) => {
 smtpTransport.close();
 
 })
-
-//keep
-// app.post('/send', async (req, res, next) => {
-  
-//   var data = req.body;
-//   console.log("this is the data alligator", JSON.stringify(data));
-//   var to = "rossicandace85@gmail.com"
-//   var subject = "Mail From Contact Form"
-//   // var name = `${data.name}`
-//   // var email = `${data.email}`
-//   // var phone = `${data.phone}`
-//   var message = `${subject} \n  name: ${data.name} \n  email: ${data.email} \n ${data.phone}`
-
-
-// var mail = {
-//     from: to,
-//     to: to,
-//     subject: subject,
-//     text: message
-//   }
-
-// transporter.sendMail(mail, (err, data) => {
-//     if (err) {
-//       res.json({
-//         status: 'fail'
-//       })
-//     } else {
-//       res.json({
-//        status: 'success'
-//       })
-//     }
-//   });
-
-//   // transporter.close();
-// });
-
 
 
 app.listen(PORT, () => console.log("listening on port 3001"));
