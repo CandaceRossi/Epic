00f4a188dc3dd689e976fbd6f3c63ab0c278367d
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2021-10-29 16:14:57 -0400

    finished styling and restyling top card responsiveness for phone and mini viewport sizes

--- epic/FE/src/components/Cards/CardsPhone.js
+++ epic/FE/src/components/Cards/CardsPhone.js
@@ -11,12 +11,14 @@ const CardsPhone = () => {
     return (
         <div className="cardscontainerphone">
             <div className="cardsstylephone">
-                <Card >
-                    <Card.Body >
+                <Card style={{  margin: "0 auto", paddingRight: "0px !important" }}>
+                    <Card.Body style={{
+                        background: "#d6cec3", color: "rgb(21, 29, 68)", paddingRight: "0px !important", margin: "0 auto !important", display: "flex !important", justifyContent: "center !important", alignItems: "center", flexDirection: "row"
+                    }}>
                         <img className="firstimg" src={beigebasement} style={{ width: "370px", height: "250px", paddingTop: "15px", display: "flex" }} alt="room" />
                         <div className="imagecard" style={{ border: "none", background: "#d6cec3", color: "rgb(21, 29, 68)", display: "flex", flexDirection: "column", justifyContent: "flex-start" }}>
-                            <Card.Title style={{ background: "#d6cec3", color: "rgb(21, 29, 68)", margin: "0 auto", maxWidth: "395px" }}>Make Your Space What You Always Dreamed It Could Be!</Card.Title>
-                            <Card.Text style={{ background: "#d6cec3", color: "rgb(21, 29, 68)", margin: "0 auto", maxWidth: "360px", fontSize: "1.2rem", display: "flex", alignItems: "flex-start" }}>
+                            <Card.Title style={{ paddingRight: "0px !important", background: "#d6cec3", color: "rgb(21, 29, 68)", margin: "0 auto", marginLeft: "0px", maxWidth: "100% !important", fontSize: "2rem" }}>Make Your Space What You Always Dreamed It Could Be!</Card.Title>
+                            <Card.Text style={{  paddingRight: "0px !important", background: "#d6cec3", color: "rgb(21, 29, 68)", margin: "0 auto", marginLeft: "0px", maxWidth: "100% !important", fontSize: "1.2rem" }}>
                                Let us do the work for you! Our team handles every step of your project so that you don't have to. We protect all surfaces, furniture, patch holes, remove electric plates and switch covers so that the finished product is beautiful with a quality and durability that lasts. 
                             </Card.Text>
 
--- epic/FE/src/components/Cards/CardsTablet.js
+++ epic/FE/src/components/Cards/CardsTablet.js
@@ -15,20 +15,19 @@ const CardsTablet = () => {
 
     return (
      
-
         <div className="cardscontainertablet">
             <div className="cardsstyletablet">
 
-                <Card style={{ border: "none", margin: "0 auto" }}>
+                <Card style={{ border: "none !important", width: "100%", margin: "0 auto", paddingRight: "0px !important" }}>
                     <Card.Body style={{
-                        background: "#d6cec3", color: "rgb(21, 29, 68)", margin: "0 auto !important", display: "flex", justifyContent: "center", alignItems: "flex-start", flexDirection: "row"
+                       border: "none", background: "#d6cec3", color: "rgb(21, 29, 68)", paddingRight: "0px !important", margin: "0 auto !important", display: "flex !important", justifyContent: "center !important", alignItems: "center", flexDirection: "row"
                     }}>
 
                      
-                        <img className="firstimg" src={beigebasement} style={{ maxWidth: "395px", height: "250px", paddingTop: "15px", display: "flex" }} alt="room" />
-                        <div className="imagecard" style={{ border: "none", margin: "0 auto", background: "#d6cec3", color: "rgb(21, 29, 68)", display: "flex", flexDirection: "column", justifyContent: "center", paddingLeft: "20px" }}>
-                            <Card.Title style={{ background: "#d6cec3", color: "rgb(21, 29, 68)", margin: "0 auto", marginLeft: "0px", maxWidth: "395px" }}>Make Your Space What You Always Dreamed It Could Be!</Card.Title>
-                            <Card.Text style={{  background: "#d6cec3", color: "rgb(21, 29, 68)", margin: "0 auto", marginLeft: "0px", maxWidth: "395px", fontSize: "1.2rem" }}>
+                        <img className="firstimg" src={beigebasement} style={{ border: "none", margin:"0 auto", width: "395px", height: "250px", paddingTop: "15px", paddingBottom: "15px", display: "flex" }} alt="room" />
+                        <div className="imagecard" style={{ border: "none", paddingRight: "0px !important", margin: "0 auto", background: "#d6cec3", color: "rgb(21, 29, 68)", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", paddingLeft: "30px", margin: "0 auto", marginRight: "0px !important" }}>
+                            <Card.Title style={{ paddingRight: "0px !important", background: "#d6cec3", color: "rgb(21, 29, 68)", margin: "0 auto", marginLeft: "0px", maxWidth: "100% !important", fontSize: "2rem" }}>Make Your Space What You Always Dreamed It Could Be!</Card.Title>
+                            <Card.Text style={{  paddingRight: "0px !important", background: "#d6cec3", color: "rgb(21, 29, 68)", margin: "0 auto", marginLeft: "0px", maxWidth: "100% !important", fontSize: "1.2rem" }}>
                                  Let us do the work for you! Our team handles every step of your project so that you don't have to. We protect all surfaces, furniture, patch holes, remove electric plates and switch covers so that the finished product is beautiful with a quality and durability that lasts. 
                             </Card.Text>
 
